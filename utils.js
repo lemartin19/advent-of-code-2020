23ce286ba001edd8d66d401071c822f33dfe32bd
Lynnsey Martin <lmartin@hubspot.com>
2020-12-12 19:51:35 -0500

    add array equality

--- utils.js
+++ utils.js
@@ -7,3 +7,9 @@ const buildArr = (size, val = 0) => {
   return arr;
 };
 exports.buildArr = buildArr;
+
+const arrayEquals = (arr1, arr2) => {
+  return arr1.length === arr2.length
+    && arr1.every((val, idx) => val === arr2[idx])
+};
+exports.arrayEquals = arrayEquals;
