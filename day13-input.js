4d75a48f5ec61027e343dd89274d78f101eda60b
Lynnsey Martin <lmartin@hubspot.com>
2020-12-13 18:19:23 -0500

    day 13

---
+++ day13-input.js
@@ -0,0 +1,9 @@
+
+const TIMESTAMP = 1001938;
+const INPUT = [41,'x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x',37,'x','x','x','x','x',431,'x','x','x','x','x','x','x',23,'x','x','x','x',13,'x','x','x',17,'x',19,'x','x','x','x','x','x','x','x','x','x','x',863,'x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x',29];
+
+const TEST_TIMESTAMP = 939;
+const TEST = [7, 13, 'x', 'x', 59, 'x', 31, 19];
+
+exports.TIMESTAMP = TIMESTAMP;
+exports.INPUT = INPUT;
---
+++ day13.js
@@ -0,0 +1,39 @@
+
+const { INPUT, TIMESTAMP } = require('./day13-input');
+
+const calcEarliestTime = earliest => busId => {
+  if (busId === 'x') return { busId, leavingAt: null };
+  const numRoutesCompleted = Math.ceil(earliest / busId);
+  return { busId, leavingAt: numRoutesCompleted * busId };
+};
+
+const firstBusAfter = (allBuses) =>
+  allBuses.reduce((minBus, currBus) => 
+    currBus.leavingAt && currBus.leavingAt < minBus.leavingAt ? currBus : minBus);
+
+const calcSequentialLeavingTime = schedule => {
+  let currTime = 0;
+  let idx = 0;
+  let increment = 1;
+
+  while (idx < schedule.length) {
+    if (schedule[idx] === 'x') idx++;
+    else if ((currTime + idx) % schedule[idx] === 0) {
+      increment *= schedule[idx];
+      idx++;
+    }
+    else {
+      currTime += increment;
+    }
+  }
+
+  return currTime;
+};
+
+const allBuses = INPUT.map(calcEarliestTime(TIMESTAMP));
+const { leavingAt, busId } = firstBusAfter(allBuses);
+const waitTime = leavingAt - TIMESTAMP;
+const specialTime = calcSequentialLeavingTime(INPUT);
+
+console.log(`result pt 1: ${waitTime * busId}`);
+console.log(`result pt 2: ${specialTime}`);
